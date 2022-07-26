34f0712711845383aa9f1965b5f57825ef492d43
ajankowy97 <agnieszka@thehumanaspect.com>
2022-07-26 18:36:01 +0200

    endpoints

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -28,5 +28,85 @@ export class OrdersService {
         upsert: true,
       });
     }
+
+    console.log(orders);
+  }
+
+  async profitable() {
+    const data = await this.orderModel.aggregate([
+      { $match: {} },
+      { $unwind: '$items' },
+      {
+        $addFields: {
+          convertedPrice: { $toDecimal: '$items.product.price' },
+          convertedQuantity: { $toInt: '$items.quantity' },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          total: { $multiply: ['$convertedPrice', '$convertedQuantity'] },
+        },
+      },
+      { $sort: { total: -1 } },
+
+      { $limit: 10 },
+    ]);
+
+    return data;
+  }
+
+  async mostBought() {
+    const data = await this.orderModel.aggregate([
+      { $unwind: '$items' },
+      {
+        $group: {
+          _id: '$items.product',
+          totalAmount: { $sum: 1 },
+        },
+      },
+      { $sort: { totalAmount: -1 } },
+
+      { $project: { _id: 1, totalAmount: 1 } },
+      { $limit: 10 },
+    ]);
+
+    return data;
+  }
+
+  async mostBoughtYesterday() {
+    const today = new Date().toDateString();
+    const toodayIso = new Date(today).toISOString();
+    const yesterday = new Date(Date.now() - 86400000).toDateString();
+    const yesterdayIso = new Date(yesterday).toISOString();
+
+    const data = await this.orderModel.aggregate([
+      {
+        $addFields: {
+          convertedDate: { $toDate: '$date' },
+        },
+      },
+      {
+        $match: {
+          date: {
+            $gte: yesterdayIso,
+            $lt: toodayIso,
+          },
+        },
+      },
+      { $unwind: '$items' },
+      {
+        $group: {
+          _id: '$items.product',
+          totalAmount: { $sum: 1 },
+        },
+      },
+      { $sort: { totalAmount: -1 } },
+      { $project: { _id: 1, totalAmount: 1, convertedDate: 1 } },
+
+      { $limit: 10 },
+    ]);
+
+    return data;
   }
 }
